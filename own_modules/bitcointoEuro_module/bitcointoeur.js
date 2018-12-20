541a070ed7ce029029dc4747ff52fd1af3acc1f7
Captain <mads130f@edu.easj.dk>
2018-12-20 10:34:47 +0100

    modulesfun

--- own_modules/bitcointoEuro_module/bitcointoeur.js
+++ own_modules/bitcointoEuro_module/bitcointoeur.js
@@ -9,10 +9,13 @@ function bitcointoeur(bitcoin){
     //url til api
     var url = "https://api.cryptonator.com/api/ticker/btc-eur"
     
+
+
     //require http request
     var xhr = require('xmlhttprequest').XMLHttpRequest
     var xmlhttp = new xhr;
     
+    var test = 0; 
     //Ved hver readystatechange den modtager, tester den om den er 4(har fået response) og at serveren siger ok(200)
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -22,11 +25,16 @@ function bitcointoeur(bitcoin){
           var myObj = JSON.parse(this.responseText);
           //printer temperaturen
           console.log("nuværende bitcoin pris i euro: " +myObj.ticker.price);
+          test = myObj.ticker.price;
+        return test;
         }
     };
-    
+
+    //åbner requesten
     xmlhttp.open("GET", url, true);
+    //sender requesten
     xmlhttp.send();
     }
+    //exporter den
     exports.omregner = bitcointoeur;
     
---
+++ own_modules/euroTodkk_module/omregneretd.js
@@ -0,0 +1,8 @@
+function eurotodkk(euro){
+    var dkk = euro*7.42; 
+    return dkk;
+    console.log(dkk)
+
+}   
+
+exports.omregner = eurotodkk;
