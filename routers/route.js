d05acda87f043dfe6d120721863b49f55268b151
Captain <mads130f@edu.easj.dk>
2018-12-20 13:50:14 +0100

    extra comments

--- routers/route.js
+++ routers/route.js
@@ -4,16 +4,19 @@
 module.exports = function (app) {
     var controller = require('../controller/controller');
     
-    // Routes
+    // route til at hente data 
     app.route('/api/hentdata')
         .get(controller.hent_data)
 
+        //route til at oprette en konto
     app.route('/api/opretKonto').post(controller.opret_konto)
     
+    //route til at oprette trans
     app.route('/api/opretTrans').post(controller.lavTrans);
 
-/*
+    //route til hent alle trans
     app.route('/api/henttrans').get(controller.hent_transfers);
-*/
+
+    //route til delete konto
     app.route('/api/deletekonto').delete(controller.deletekonto);
 };
